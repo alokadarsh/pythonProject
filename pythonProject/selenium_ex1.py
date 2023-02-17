caf1f1b3c14ba0c6672709fc4fc6873fd357e1a2
alokadarsh <alok29adarsh10@gmail.com>
2023-02-17 23:47:59 +0530

    code done

--- pythonProject/selenium_ex1.py
+++ pythonProject/selenium_ex1.py
@@ -1,5 +1,4 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
@@ -8,8 +7,14 @@ from selenium.webdriver.common.by import By
 service_obj = Service("C:/Users/Alok/drivers/chromedriver_win32/chromedriver.exe")
 driver = webdriver.Chrome(service=service_obj)
 driver.get("https://youtube.com/")
+time.sleep(4)
 driver.maximize_window()
+time.sleep(4)
 
 driver.find_element(By.XPATH,"//input[@id='search']").send_keys("tujhe dekha to ye jana sanam")
 time.sleep(4)
-time.sleep(40)
+driver.find_element(By.XPATH,"//button[@id='search-icon-legacy']").click()
+time.sleep(4)
+driver.find_element(By.XPATH,"//a[@id='video-title']").click()
+time.sleep(140)
+
