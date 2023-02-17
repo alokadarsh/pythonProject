2e0b7edbccb286cf9208f55c4500804f2651a9b9
alokadarsh <alok29adarsh10@gmail.com>
2023-02-18 01:25:18 +0530

    code done

--- pythonProject/python oops concept/oops_fun1.py
+++ pythonProject/python oops concept/oops_fun1.py
@@ -4,4 +4,26 @@ def test():
     sum=a+b
     print(sum)
 test()
-test()
+
+
+def test2(a,b):
+    sum=a+b
+    sub=a-b
+    print(sum,sub)
+test2(68,98)
+
+def test3(a=14,b=9):
+    sum=a+b
+    sub=a-b
+    print(sum,sub)
+test3()
+
+
+def test4(a=14, b=98):
+    sum = a + b
+    sub = a - b
+    print(sum, sub)
+
+
+test4()
+test4(87,65)
