fc39b01a3ff360d31852d9ae031cafc72840f9dc
alokadarsh <alok29adarsh10@gmail.com>
2023-02-18 10:31:47 +0530

    code done

--- pythonProject/python oops concept/oops_fun1.py
+++ pythonProject/python oops concept/oops_fun1.py
@@ -1,29 +1,40 @@
+print("test case1")
 a=6
 b=8
-def test(a,b):
+def test():
     sum=a+b
     print(sum)
 test()
 
-
+print("test case 2")
 def test2(a,b):
     sum=a+b
     sub=a-b
     print(sum,sub)
 test2(68,98)
-
+print("test case 3")
 def test3(a=14,b=9):
     sum=a+b
     sub=a-b
     print(sum,sub)
 test3()
 
-
+print("test case 4")
 def test4(a=14, b=98):
     sum = a + b
     sub = a - b
     print(sum, sub)
 
-
 test4()
 test4(87,65)
+print("test case 5")
+a=6
+b=8
+def test(a=6,b=4):
+    sum=a+b
+    print(sum)
+
+test(5,6)
+test()
+test()
+test2(4555,6)
